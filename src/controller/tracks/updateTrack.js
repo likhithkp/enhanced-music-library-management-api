a59282e524edde44c603e14ed0de5049a35c72bb
likhithkp <likhithkpdev@gmail.com>
2024-12-19 18:19:30 +0530

    Corrected error messages

--- src/controller/tracks/updateTrack.js
+++ src/controller/tracks/updateTrack.js
@@ -19,7 +19,7 @@ const updateTrack = async (req, res) => {
             return res.status(404).json({
                 "status": 404,
                 "data": null,
-                "message": "Track Not Found",
+                "message": "Track doesnt exist",
                 "error": null
             });
         }
