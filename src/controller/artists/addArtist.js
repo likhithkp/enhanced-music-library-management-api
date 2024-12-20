a1f35909ce40e57360efe1f25111c0dcffccbae2
likhithkp <likhithkpdev@gmail.com>
2024-12-20 13:34:27 +0530

    Added bad request and fixed the logic in add artist

--- src/controller/artists/addArtist.js
+++ src/controller/artists/addArtist.js
@@ -16,7 +16,7 @@ const addArtist = async (req, res) => {
         }
 
         // Check if the user already exists with the provided email
-        const artist = await getArtist({name: name});
+        const artist = await getArtist({ name: name });
         if (artist) {
             return res.status(409).json({
                 "status": 409,
@@ -27,11 +27,19 @@ const addArtist = async (req, res) => {
         }
 
         const newArtist = await createArtist({ name, grammy, hidden });
-        if (newArtist) {
+        console.log(newArtist)
+        if (!newArtist.error) {
             return res.status(201).json({
                 "status": 201,
                 "data": null,
                 "message": "Artist created successfully.",
+                "error": null
+            });
+        } else {
+            return res.status(400).json({
+                "status": 400,
+                "data": null,
+                "message": "Bad Request",
                 "error": null
             });
         }
